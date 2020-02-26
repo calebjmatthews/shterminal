016daad3246326a3ab54512501be9519afd5e5b3
Caleb Matthews <calebjmatthews@gmail.com>
2020-02-26 12:23:36 -0600

    Time passes in header clock

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
-export default function Header() {
-  return (
-    <div className="header">
-      <div>
-        GOC SH TERMINAL v108.55
+export default class Header extends Component {
+  state: HeaderState;
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      secondsElapsed: 0,
+      interval: setInterval(() => {
+        this.countSecond();
+      }, 1000),
+    }
+  }
+
+  countSecond() {
+    this.setState({ secondsElapsed: this.state.secondsElapsed+1 });
+  }
+
+  render() {
+    let startTime = new Date(Date.now());
+    let storyMoment = moment('82-05-05 02:33:06', 'YY-MM-DD HH:mm:ss');
+    storyMoment.add(this.state.secondsElapsed, 'seconds');
+    let datetimeText = storyMoment.format('YY-MM-DD HH:mm:ss');
+    return (
+      <div className="header">
+        <div>
+          GOC SH TERMINAL v108.55
+        </div>
+        <div>
+          LOGGED IN AS matalanta
+        </div>
+        <div>
+          {datetimeText}
+        </div>
       </div>
-      <div>
-        LOGGED IN AS matalanta
-      </div>
-      <div>
-        82-05-05 02:33:06
-      </div>
-    </div>
-  );
+    );
+  }
+}
+
+class HeaderState {
+  secondsElapsed: number;
+  interval: NodeJS.Timeout;
 }
